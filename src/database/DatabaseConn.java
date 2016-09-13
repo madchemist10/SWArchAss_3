a068f8c66e2169b2cf7232253e7f7e16acf8779e
madchemist10 <2012kemp1234@gmail.com>
2016-09-13 14:28:16 -0500

    update javadocs for databaseConn class.

--- src/database/DatabaseConn.java
+++ src/database/DatabaseConn.java
@@ -8,33 +8,46 @@ import java.util.ArrayList;
  * Database class responsible for database connection establishment.
  * http://www.tutorialspoint.com/sqlite/sqlite_java.htm
  */
-public class DatabaseConn {
+class DatabaseConn {
+
+    /**Reference to the connection for this database connection.*/
     private Connection conn = null;
+    /**Database filepath for which this connection is connecting to.*/
     private String database = null;
 
-    public DatabaseConn(String db){
+    /**
+     * Create and establish a new connection to the database.
+     * @param db String representation of the filepath to the database.
+     */
+    DatabaseConn(String db){
         this.database = db;
         this.conn = this.makeConnection(this.database);
     }
 
-    public void setConn(Connection conn) {
-        this.conn = conn;
-    }
-
-    public Connection getConn() {
-        return conn;
-    }
-
+    /**
+     * Set the database filepath.
+     * @param database filepath to the database.
+     */
     public void setDatabase(String database) {
         this.database = database;
     }
 
+    /**
+     * Retrieve the filepath to the database.
+     * @return String representation of the filepath of this connection's database.
+     */
     public String getDatabase() {
         return database;
     }
 
-    //make connection
-    public Connection makeConnection(String database){
+    /**
+     * Establish a connection to an sqlite database.
+     * Uses sqlite library.
+     * @param database filepath to the database.
+     * @return Connection that has been established or null
+     *      if an error occurred.
+     */
+    private Connection makeConnection(String database){
         try{
             Class.forName("org.sqlite.JDBC");
             return DriverManager.getConnection("jdbc:sqlite:"+database);
@@ -44,8 +57,12 @@ public class DatabaseConn {
         return null;
     }
 
-    //attempt to close connection
-    public void closeConnection(){
+    /**
+     * Attempt to close the current connection.
+     * Could result in null pointer if connection was
+     * failed to be established.
+     */
+    private void closeConnection(){
         try{
             this.conn.close();
         }catch (Exception e){
@@ -53,19 +70,31 @@ public class DatabaseConn {
         }
     }
 
-    //verify connection
-    public Integer verifyConnection(){
+    /**
+     * Ensure the connection is alive, if connection
+     * does not exist, reconnect.
+     * @return 1 if connection if connected,
+     *          0 if not connected.
+     */
+    private Integer verifyConnection(){
         if (this.conn == null){
             this.conn = this.makeConnection(this.database);
-            if (this.conn != null)
+            if (this.conn != null) {
                 return 1;
-            else
+            }else {
                 return 0;
+            }
         }
         return 1;
     }
-    //make table
-    public void makeTable(String table){
+
+    /**
+     * Create a table from statement.
+     * @param table full formatted sql query for
+     *              creating a table with columns
+     *              and data types.
+     */
+    void makeTable(String table){
         Connection conn = this.conn;
         if (this.verifyConnection() == 1){
             try{
@@ -79,8 +108,13 @@ public class DatabaseConn {
         }
     }
 
-    //insert into table
-    public void insertIntoTable(String insert){
+    /**
+     * Insert into the table, a set of values for a
+     * given set of columns.
+     * @param insert full formatted sql query for inserting
+     *               data into a table.
+     */
+    void insertIntoTable(String insert){
         Connection conn = this.conn;
         if (this.verifyConnection() == 1){
             try{
@@ -96,8 +130,13 @@ public class DatabaseConn {
         }
     }
 
-    //select from table
-    public ArrayList<String[]> selectFromTable(String selectStatement){
+    /**
+     * Select data from a table in the database.
+     * @param selectStatement full formatted query to select
+     *                        data from a table in the database.
+     * @return list of entries reported by the query.
+     */
+    ArrayList<String[]> selectFromTable(String selectStatement){
         ArrayList<String[]> returnData = new ArrayList<>();
         Connection conn = this.conn;
         if (this.verifyConnection() == 1) {
@@ -123,8 +162,12 @@ public class DatabaseConn {
         return returnData;
     }
 
-    //update table entry
-    public void updateTableEntry(String updateStatement){
+    /**
+     * Update an entry in a given table from this database connection.
+     * @param updateStatement full formatted sqlite statement
+     *                        for updating an entry in a given table.
+     */
+    void updateTableEntry(String updateStatement){
         Connection conn = this.conn;
         if (this.verifyConnection() == 1) {
             try {
