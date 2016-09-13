c1281ce3ffd25ed28c9bb3d2b7feacefcd6a1845
madchemist10 <2012kemp1234@gmail.com>
2016-09-13 14:30:22 -0500

    updated scope of statements from statement builder.

--- src/database/DBStatementBuilder.java
+++ src/database/DBStatementBuilder.java
@@ -3,39 +3,33 @@ package database;
 /**
  * Instantiates Statements for Database
  */
-public class DBStatementBuilder {
+class DBStatementBuilder {
 
-    public static String selectStatement(String selectVar){
+    static String selectStatement(String selectVar){
         return "SELECT "+selectVar;
     }
-    public static String updateStatement(String updateVar){
+    static String updateStatement(String updateVar){
         return "UPDATE "+updateVar;
     }
-    public static String insertStatement(String tableName, String insertVar){
+    static String insertStatement(String tableName, String insertVar){
         return "INSERT INTO "+tableName+" ("+insertVar+")";
     }
-    public static String whereStatement(String whereVar){
+    static String whereStatement(String whereVar){
         return " WHERE "+whereVar;
     }
-    public static String fromStatement(String fromVar){
+    static String fromStatement(String fromVar){
         return " FROM "+fromVar;
     }
-    public static String valueStatement(String valueVar){
+    static String valueStatement(String valueVar){
         return " VALUES "+"("+valueVar+")";
     }
-    public static String andStatement(String andVar){
-        return " AND "+andVar;
-    }
-    public static String setStatement(String setVar){
+    static String setStatement(String setVar){
         return " SET "+setVar;
     }
-    public static String groupByStatement(String groupByVar){
-        return " GROUP BY "+groupByVar;
-    }
-    public static String createTableStatement(String tableName){
+    static String createTableStatement(String tableName){
         return "CREATE TABLE IF NOT EXISTS '"+tableName+"'";
     }
-    public static String createTableTextColumn(String column){
+    static String createTableTextColumn(String column){
         return "\""+column+"\" TEXT";
     }
 
